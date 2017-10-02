96ce480e33eb8d5898e3fffb05e988f4707d683e
Håkan Axelsson <han@qlik.com>
2017-10-02 13:17:30 +0200

    Browser coverage (#55)
    * feat(coverage): initial browser coverage
    
    * Use requirejsStartPath and systemjsStartPath to black list instrumentation
    
    * coverage updates
    
    * Cleanup before every run
    
    * Add the `--coverage-black-list` config
    
    * Remove `useColors` since server logging is not used
    
    * Fix for getting correct URL to phantomjs
    
    * Clean up

--- src/browser/browser-test-runner-setup.js
+++ src/browser/browser-test-runner-setup.js
@@ -3,25 +3,27 @@ mocha.delay(true); // Adds a global `run` function
 // mocha.bail( true );
 mocha.fullTrace(true);
 mocha.timeout(0);
+mocha.ui('bdd');
+
 // Handle `phantomjs`
 if (typeof callPhantom === 'function') {
   Mocha.reporters.Base.useColors = true;
-  Mocha.process.stdout.write = function () {};
+  Mocha.process.stdout.write = function () { };
   console.log = function () {
     callPhantom({ args: [].slice.call(arguments) });
   };
   mocha.reporter('min');
 }
 
-mocha.ui('bdd');
+var runner = mocha.run(); //eslint-disable-line
 
-const runner = mocha.run();
 runner.on('end', () => {
   if (typeof callPhantom === 'function') {
     callPhantom({ exit: true, failures: runner.failures });
   }
 });
 
+// Hook up errors and send to server
 if (typeof callPhantom === 'function') {
   onerror = function () { // eslint-disable-line no-restricted-globals
     const args = [].slice.call(arguments);
@@ -29,3 +31,20 @@ if (typeof callPhantom === 'function') {
     callPhantom({ exit: true, failures: runner.failures });
   };
 }
+
+// eslint-disable-next-line
+runner.on('start', function() {
+  window.___browserSync___.socket.emit('runner-start'); // eslint-disable-line
+});
+
+runner.on('end', function () {
+  var coverage = window.__coverage__; //eslint-disable-line
+  if (coverage) {
+    window.___browserSync___.socket.emit('runner-end', { stats: this.stats, coverageObj: coverage }); //eslint-disable-line
+  }
+});
+
+window.onerror = function () {
+  console.log([].slice.call(arguments));
+  window.___browserSync___.socket.emit('window-error'); //eslint-disable-line
+};
---
+++ src/browser/disable-cache.js
@@ -0,0 +1,6 @@
+export default function disableCache(req, res, next) {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+}
