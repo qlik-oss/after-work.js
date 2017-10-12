39bde026199da9b29d2320bcdcabed78f203fb09
Håkan Axelsson <han@qlik.com>
2017-10-12 10:40:27 +0200

    Fix for phantomjsSingleRun

--- src/phantomjs-runner.js
+++ src/phantomjs-runner.js
@@ -5,7 +5,7 @@ import system from 'system';  //eslint-disable-line
 const page = webpage.create();
 const { args } = system;
 const url = args[args.indexOf('--pageUrl') + 1];
-const singleRun = args[args.indexOf('--single-run') + 1] === 'true';
+// const singleRun = args[args.indexOf('--single-run') + 1] === 'true';
 
 page.open(url, (status) => {
   if (status === 'fail') {
@@ -14,10 +14,6 @@ page.open(url, (status) => {
   }
 
   page.onCallback = (data) => {
-    if (data.exit && singleRun) {
-      const exitCode = data.failures > 0 ? 1 : 0;
-      phantom.exit(exitCode); // eslint-disable-line no-undef
-    }
     let msg = util.format(...data.args);
     msg = msg.replace('\u2713', 'v');
     msg = msg.replace(/[\w-]+\.spec\.js/, '\u001b[0m\u001b[31m$&\u001b[90m');
