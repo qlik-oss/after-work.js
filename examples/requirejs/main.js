818b7a3a9ae3430f5af4959d4141579e9d5fe8fb
Christoffer Åström <cam@qlik.com>
2017-10-27 09:32:30 +0200

    Make the example `dynamic` e.g use `window.awFiles`

--- examples/requirejs/main.js
+++ examples/requirejs/main.js
@@ -4,7 +4,7 @@
       chai: 'https://cdnjs.cloudflare.com/ajax/libs/chai/4.1.2/chai.min',
     },
   });
-  requirejs(['./test.spec.js'], () => {
+  requirejs(window.awFiles, () => {
     mocha.run();
   });
 }());
