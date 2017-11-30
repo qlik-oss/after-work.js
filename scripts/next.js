ba7adea90d9088549f383cc3c135b927f8a8ea99
Håkan Axelsson <han@qlik.com>
2017-11-30 13:15:43 +0100

    Adjusting date format

--- scripts/next.js
+++ scripts/next.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const pkg = require('../package');
+const moment = require('moment');
 const { execSync } = require('child_process');
 
-const nextVersion = `${pkg.version.split('-dev').shift()}-dev.${+new Date()}`;
+const nextVersion = `${pkg.version.split('-dev').shift()}-dev.${+moment().format('YYYYMMDD')}`;
 execSync(`npm version ${nextVersion} --no-git-tag-version`);
