fa9b9b031d8b27ce3e2f860b95c837766ce7cd8a
Christoffer Åström <cam@qlik.com>
2018-10-17 11:10:05 +0200

    refactor(example-react): rename files to jsx

--- examples/react/test/fancy-button.spec.js
+++ examples/react/test/fancy-button.spec.js
@@ -7,8 +7,14 @@ import FancyButton from '../src/fancy-button';
 
 describe('FancyButton', () => {
   it('renders fancy with button as span or div', () => {
-    const [{ default: FancySpan }] = aw.mock([['**/react/src/button.js', '() => (<span>hhhhh</span>)']], ['../src/fancy-button']);
-    const [{ default: FancyDiv }] = aw.mock([['**/react/src/button.js', '() => (<div>ggggg</div>)']], ['../src/fancy-button']);
+    const [{ default: FancySpan }] = aw.mock(
+      [['**/react/src/button.jsx', '() => (<span>hhhhh</span>)']],
+      ['../src/fancy-button'],
+    );
+    const [{ default: FancyDiv }] = aw.mock(
+      [['**/react/src/button.jsx', '() => (<div>ggggg</div>)']],
+      ['../src/fancy-button'],
+    );
     const tree1 = renderer.create(<FancySpan>span</FancySpan>).toJSON();
     const tree2 = renderer.create(<FancyDiv>div</FancyDiv>).toJSON();
     expect(tree1).toMatchSnapshot();
@@ -27,8 +33,13 @@ describe('FancyButton', () => {
   });
 
   it('renders fancy with button mock from file', () => {
-    const [{ default: FancyFileMock }] = aw.mock([['**/react/src/button.js', './examples/react/test/button-mock.js']], ['../src/fancy-button']);
-    const tree1 = renderer.create(<FancyFileMock>file mock</FancyFileMock>).toJSON();
+    const [{ default: FancyFileMock }] = aw.mock(
+      [['**/react/src/button.jsx', './examples/react/test/button-mock.js']],
+      ['../src/fancy-button'],
+    );
+    const tree1 = renderer
+      .create(<FancyFileMock>file mock</FancyFileMock>)
+      .toJSON();
     expect(tree1).toMatchSnapshot();
   });
 });
