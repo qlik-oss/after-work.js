75c59588d509c5e8d2707521a8031db0743cbf2d
Christoffer Åström <cam@qlik.com>
2019-01-10 15:23:19 +0100

    fix(protractor): add back options to gethelp output

--- commands/protractor/src/index.js
+++ commands/protractor/src/index.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const importCwd = require('import-cwd');
+const options = require('./options');
 
 const protractor = {
   command: ['protractor', 'ptor'],
   desc: 'Run protractor',
+  builder(yargs) {
+    return yargs.options(options);
+  },
   handler() {
     const launcherPath = 'protractor/built/launcher';
     const launcher = importCwd.silent(launcherPath) || require(launcherPath);
