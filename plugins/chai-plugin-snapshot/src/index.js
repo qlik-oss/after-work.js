91935208cfe031238eb9adb7de0455eabfcc2a6a
Miralem Drek <mek@qlik.com>
2019-02-21 16:58:42 +0100

    fix: reduce risk of falsy test case match

--- plugins/chai-plugin-snapshot/src/index.js
+++ plugins/chai-plugin-snapshot/src/index.js
@@ -26,7 +26,7 @@ module.exports = function snapshot(runner) {
     let currentTestName = null;
     for (let i = lineno - 1; i >= 0; i -= 1) {
       const line = lines[i];
-      if (line.trimLeft().startsWith('it')) {
+      if (line.trimLeft().startsWith('it(')) {
         [, currentTestName] = line.match(/it.*\((.*),/);
         break;
       }
