3347ed336a133ce6669b10ac9efa74da1f879069
stoffeastrom <stoffeastrom@gmail.com>
2019-03-08 11:53:01 +0100

    chore: add self mock for test

--- examples/react/test/button-hook.spec.js
+++ examples/react/test/button-hook.spec.js
@@ -1,12 +1,19 @@
 /* global aw */
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import Button from '../src/button-hook';
 
 describe('Button with hook', () => {
   it('renders', () => {
     const [{ default: ButtonHook }] = aw.mock([], ['../src/button-hook']);
     const tree1 = renderer.create(<ButtonHook>hook</ButtonHook>).toJSON();
+    expect(tree1).toMatchSnapshot();
+  });
+  it('renders button hook', () => {
+    const [ButtonHook] = aw.mock(
+      [['**/react/src/button-hook.jsx', () => () => <span>hook</span>]],
+      ['../src/button-hook'],
+    );
+    const tree1 = renderer.create(<ButtonHook>span</ButtonHook>).toJSON();
     expect(tree1).toMatchSnapshot();
   });
 });
