88dcf8c10ea2979a660fc67d89070ea7f8ab034f
Håkan Axelsson <han@qlik.com>
2019-03-26 21:06:20 +0100

    Update hello.spec.js

--- examples/protractor/test/hello.spec.js
+++ examples/protractor/test/hello.spec.js
@@ -1,5 +1,6 @@
 describe('Protractor', () => {
   it('should say hello world', async () => {
+    browser.waitForAngularEnabled(false);
     await browser.get('/examples/protractor/test/hello.fix.html');
     const container = await element(by.id('container'));
     const txt = await container.getText();
